d5d203b7f3d44b0fc03f33b9342b8e641b9c9751
Kiru <kirushanthh13@gmail.com>
2025-05-29 09:35:42 +0530

    Backend almost done

--- src/main/java/com/kirushh/movies/Review.java
+++ src/main/java/com/kirushh/movies/Review.java
@@ -1,4 +1,24 @@
 package com.kirushh.movies;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(collection = "reviews")
 public class Review {
+
+    @Id
+    private ObjectId id;
+
+    private String body;
+
+    public Review(String body) {
+        this.body = body;
+    }
 }
