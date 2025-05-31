74e75b5002cb06ed1c010afea87fadde8767322b
Kiru <kirushanthh13@gmail.com>
2025-05-31 15:10:26 +0530

    Finshed all the features

--- client-frontend/movie/src/components/hero/Hero.js
+++ client-frontend/movie/src/components/hero/Hero.js
@@ -2,6 +2,10 @@ import "./Hero.css";
 import { Paper } from "@mui/material";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // required CSS
 import { Carousel } from "react-responsive-carousel";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCirclePlay } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
 
 const Hero = ({ movies }) => {
   return (
@@ -11,12 +15,12 @@ const Hero = ({ movies }) => {
         showStatus={false}
         infiniteLoop
         autoPlay
-        interval={5000}
+        interval={2000}
         transitionTime={600}
         emulateTouch
       >
-        {movies?.map((movie, index) => (
-          <Paper key={index}>
+        {movies?.map((movie) => (
+          <Paper key={movie.imdbId}>
             <div className="movie-card-container">
               <div
                 className="movie-card"
@@ -28,6 +32,29 @@ const Hero = ({ movies }) => {
                   </div>
                   <div className="movie-title">
                     <h4>{movie.title}</h4>
+                  </div>
+                  <div className="movie-buttons-container">
+                    <Link
+                      to={`/Trailer/${movie.trailerLink.substring(
+                        movie.trailerLink.length - 11
+                      )}`}
+                    >
+                      <div className="play-button-icon-container">
+                        <FontAwesomeIcon
+                          className="play-button-icon"
+                          icon={faCirclePlay}
+                        />
+                      </div>
+                    </Link>
+
+                    <div className="movie-review-button-container">
+                      <Button
+                        variant="info"
+                        onClick={() => reviews(movie.imdbId)}
+                      >
+                        Reviews
+                      </Button>
+                    </div>
                   </div>
                 </div>
               </div>
